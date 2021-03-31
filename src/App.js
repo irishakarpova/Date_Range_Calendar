7aaf1c5e5cae704e13d1d3d1bb197214b2139922
irina <irisha.karpova@gmail.com>
2021-03-31 13:29:41 -0500

    update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
     return (
         <div className="container">
             <Provider store={store}>
-                <DayPicker />
                 <ArticleList />
             </Provider>
         </div>
