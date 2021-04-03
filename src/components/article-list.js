86aa7aad7f820d316b12555ed03f6ca200a24f09
irina <irisha.karpova@gmail.com>
2021-04-03 16:01:18 -0500

    add handlescroll listener

--- src/components/article-list.js
+++ src/components/article-list.js
@@ -2,24 +2,47 @@ import React, { Component } from "react";
 import Article from "./article";
 import { connect } from "react-redux";
 import { filtratedArticleSelector } from "../selectors";
-import classes from "./styles.module.css";
+import styles from "./styles.module.css";
 import Accordion from "./accordion";
 
 class ArticleList extends Component {
+    state = {
+        showItems: 4
+    };
+
+    handleScroll = () => {
+        if (
+            window.innerHeight + window.pageYOffset >=
+            document.body.offsetHeight
+        ) {
+            this.setState({ showItems: this.state.showItems + 5 });
+        }
+    };
+
+    componentDidMount() {
+        window.addEventListener("scroll", this.handleScroll);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("scroll", this.handleScroll);
+    }
+
     render() {
         return (
-            <div className={classes.CardColumns}>
-                <ul className={classes.Cardlist}>{this.itemsListLeft}</ul>
-                <ul className={classes.Cardlist}>{this.itemsListRight}</ul>
-            </div>
+            <>
+                <div className={styles.CardColumns}>
+                    <ul className={styles.Cardlist}>{this.itemsListLeft}</ul>
+                    <ul className={styles.Cardlist}>{this.itemsListRight}</ul>
+                </div>
+            </>
         );
     }
 
     get itemsListLeft() {
         const { articles, openArticleId, handleOpenArticleId } = this.props;
-        return articles.map((article, index) => {
+        return articles.slice(0, this.state.showItems).map((article, index) => {
             return index % 2 === 0 ? (
-                <li className={classes.CardListItems} key={article.id}>
+                <li className={styles.CardListItems} key={article.id}>
                     <Article
                         article={article}
                         openArticleId={openArticleId}
@@ -29,12 +52,11 @@ class ArticleList extends Component {
             ) : null;
         });
     }
-
     get itemsListRight() {
         const { articles, openArticleId, handleOpenArticleId } = this.props;
-        return articles.map((article, index) => {
+        return articles.slice(0, this.state.showItems).map((article, index) => {
             return (index + 1) % 2 === 0 ? (
-                <li className={classes.CardListItems} key={article.id}>
+                <li className={styles.CardListItems} key={article.id}>
                     <Article
                         article={article}
                         openArticleId={openArticleId}
